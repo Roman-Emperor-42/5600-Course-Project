a8df116188cbdbc68f6f9547fc0990ff432593b4
caleb <graham.caleb.m@gmail.com>
2025-12-03 11:52:20 -0600

    Better comments

--- Warm-Up/src/client.java
+++ Warm-Up/src/client.java
@@ -3,12 +3,10 @@ import java.net.*;
 import java.util.Scanner;
 
 public class client {
-
     private Socket s = null;
-    private DataOutputStream out = null;
+    private DataOutputStream ServerOut = null;
     private DataInputStream serverIn = null;
     private static Scanner mainScanner = null;
-
 
     // Try to get machine's Host name, if fail throw exception
     private static String getHost() {
@@ -16,7 +14,9 @@ public class client {
         try {
             InetAddress ipAddress = InetAddress.getLocalHost();
             hostname = ipAddress.getHostName();
-        } catch (UnknownHostException e) {
+        }
+        // Catch exceptions
+        catch (UnknownHostException e) {
             System.err.println("Could not determine local host information: " + e.getMessage());
             e.printStackTrace();
         }
@@ -29,7 +29,9 @@ public class client {
         try {
             InetAddress ipAddress = InetAddress.getLocalHost();
             ipString = ipAddress.getHostAddress();
-        } catch (UnknownHostException e) {
+        }
+        // Catch exceptions
+        catch (UnknownHostException e) {
             System.err.println("Could not determine local host information: " + e.getMessage());
             e.printStackTrace();
         }
@@ -43,87 +45,97 @@ public class client {
             System.out.println("Connected");
 
             // Sends output to the socket
-            out = new DataOutputStream(s.getOutputStream());
-
+            ServerOut = new DataOutputStream(s.getOutputStream());
             serverIn = new DataInputStream(s.getInputStream());
         }
+        // Print Unknown Host
         catch (UnknownHostException u) {
             System.out.println(u);
             return;
         }
+        // Print Error
         catch (IOException i) {
             System.out.println(i);
             return;
         }
 
-        // String to read message from input
+        // Send hellow message
         String m = "Hello from Client-" + getHost();
         try {
-            out.writeUTF(m);
-            //System.out.println("Client: " + m);
 
+            ServerOut.writeUTF(m);
+
+            // Echo server response
             String serverResponse = serverIn.readUTF();
             System.out.println("Server: " + serverResponse);
-        }
-        catch (IOException i) {
+        } catch (IOException i) {
             System.out.println(i);
         }
 
         // Java is quirky and will auto send a blank line if you don't take an input before ¯\O/¯ (this is a shrugging emoji cause idk why it does thisz)
         String userInput = mainScanner.nextLine();
 
-        // Keep reading until "Over" is input
-        while(true) {
+        // Keep reading until "Bye from Client-'your hostname'" is input
+        while (true) {
             try {
+                // Take input
                 System.out.print("Enter message (or type 'Bye from Client-" + getHost() + "' to quit): ");
                 userInput = mainScanner.nextLine();
 
+                // Catch empty message
                 if (userInput.trim().isEmpty()) {
                     System.out.println("Please enter a message.");
                     continue;
                 }
 
-                out.writeUTF(userInput);
-                //System.out.println("Client: " + userInput);
+                //Send message
+                ServerOut.writeUTF(userInput);
 
+                // Disconnect
                 if (userInput.startsWith("Bye from Client-" + getHost())) {
                     String serverBye = serverIn.readUTF();
                     System.out.println("Server: " + serverBye);
                     break;
                 }
 
+                // Echo response from server
                 String echo = serverIn.readUTF();
                 System.out.println(echo);
             }
+            // Catch exceptions
             catch (IOException i) {
                 System.out.println(i);
                 break;
             }
         }
 
-        // Close the connection
+        // Close the connections
         try {
             mainScanner.close();
             serverIn.close();
-            out.close();
+            ServerOut.close();
             s.close();
         }
+        // Catch errors
         catch (IOException i) {
             System.out.println(i);
         }
     }
 
     public static void main(String[] args) {
+        // Print out Ip and Hostname to differentiate computers
         System.out.println("Your IP is: " + getIP());
         System.out.println("Your hostname is: " + getHost());
+
+        // Scanner to get IP and port
         mainScanner = new Scanner(System.in);
         System.out.println("Enter IP Address:");
         String ip = mainScanner.nextLine();
-
         System.out.println("Enter Port:");
         int port = mainScanner.nextInt();
-        //mainScanner.close();
 
+        // Start client
         client c = new client(ip, port);
+        mainScanner.close();
     }
 }
