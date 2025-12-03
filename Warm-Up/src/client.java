3490bd55121f864235a9d8d4169f6a9bc367688a
caleb <graham.caleb.m@gmail.com>
2025-12-02 19:07:40 -0600

    Initial working client server

--- Warm-Up/src/client.java
+++ Warm-Up/src/client.java
@@ -1,37 +1,88 @@
-package client;
-
-import java.net.*;
 import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 
-public class GreetServer {
-    private ServerSocket serverSocket;
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
+public class client {
 
-    public void start(int port) {
-        serverSocket = new ServerSocket(port);
-        clientSocket = serverSocket.accept();
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        String greeting = in.readLine();
-        if("hello server".equals(greeting)) {
-            out.println("hello client");
+    private Socket s = null;
+    private DataInputStream in = null;
+    private DataOutputStream out = null;
+
+
+    private static String getHost() {
+        // Try to get machine's IP address, if fail throw exception
+        String hostname = null;
+        try {
+            InetAddress ipAddress = InetAddress.getLocalHost();
+            hostname = ipAddress.getHostName();
+        } catch (UnknownHostException e) {
+            System.err.println("Could not determine local host information: " + e.getMessage());
+            e.printStackTrace();
         }
-        else{
-            out.println("unrecognised greeting");
+        return hostname;
+    }
+
+    public client(String addr, int port)
+    {
+        // Establish a connection
+        try {
+            s = new Socket(addr, port);
+            System.out.println("Connected");
+
+            // Takes input from terminal
+            in = new DataInputStream(System.in);
+
+            // Sends output to the socket
+            out = new DataOutputStream(s.getOutputStream());
+        }
+        catch (UnknownHostException u) {
+            System.out.println(u);
+            return;
+        }
+        catch (IOException i) {
+            System.out.println(i);
+            return;
         }
 
-        public void stop() {
+        // String to read message from input
+        String m = "Hello from Client-" + getHost();
+        try {
+            out.writeUTF(m);
+        }
+        catch (IOException i) {
+            System.out.println(i);
+        }
+
+        // Keep reading until "Over" is input
+        while (!m.equals("Over")) {
+            try {
+                m = in.readLine();
+                out.writeUTF(m);
+            }
+            catch (IOException i) {
+                System.out.println(i);
+            }
+        }
+
+        // Close the connection
+        try {
             in.close();
             out.close();
-            clientSocket.close();
-            serverSocket.close();
+            s.close();
         }
+        catch (IOException i) {
+            System.out.println(i);
+        }
+    }
 
-        public static void main(String[] args) {
-            GreetServer server = new GreetServer();
-            server.start(6666);
-        }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter IP Address:");
+        String ip = in.nextLine();
+
+        System.out.println("Enter Port:");
+        int port = in.nextInt();
+
+        client c = new client(ip, port);
     }
 }
--- Warm-Up/src/server.java
+++ Warm-Up/src/server.java
@@ -1,28 +1,96 @@
-package server;
-
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
 
 public class server {
-    private Socket clientSocket;
-    private PrintWriter out;
-    private BufferedReader in;
 
-    public void startConnection(String ip, int port) {
-        clientSocket = new Socket(ip, port);
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    // Initialize socket and input stream
+    private Socket s = null;
+    private ServerSocket ss = null;
+    private DataInputStream in = null;
+
+    private static String getIP() {
+        // Try to get machine's IP address, if fail throw exception
+        String ipString = null;
+        try {
+            InetAddress ipAddress = InetAddress.getLocalHost();
+            ipString = ipAddress.getHostAddress();
+        } catch (UnknownHostException e) {
+            System.err.println("Could not determine local host information: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return ipString;
     }
 
-    public String sendMessage(String msg) {
-        out.println(msg);
-        String resp = in.readLine();
-        return resp;
+    private static String getHost() {
+        // Try to get machine's IP address, if fail throw exception
+        String hostname = null;
+        try {
+            InetAddress ipAddress = InetAddress.getLocalHost();
+            hostname = ipAddress.getHostName();
+        } catch (UnknownHostException e) {
+            System.err.println("Could not determine local host information: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return hostname;
     }
 
-    public void stopConnection() {
-        in.close();
-        out.close();
-        clientSocket.close();
+    // Constructor with port
+    public server(int port) {
+
+        // Starts server and waits for a connection
+        try
+        {
+            ss = new ServerSocket(port);
+            System.out.println("Server started");
+
+            System.out.println("Waiting for a client ...");
+
+            s = ss.accept();
+            System.out.println("Client accepted");
+
+            // Takes input from the client socket
+            in = new DataInputStream(
+                    new BufferedInputStream(s.getInputStream()));
+
+            String m = "Hello from Server-" + getHost();
+            System.out.println(m);
+
+            // Reads message from client until "Over" is sent
+            while (!m.equals("Over"))
+            {
+                try
+                {
+                    m = in.readUTF();
+                    System.out.println(m);
+
+                }
+                catch(IOException i)
+                {
+                    System.out.println(i);
+                }
+            }
+            System.out.println("Closing connection");
+
+            // Close connection
+            s.close();
+            in.close();
+        }
+        catch(IOException i)
+        {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String args[]) {
+
+        System.out.println("Local IP Address: " + getIP());
+
+        // what port to broadcast on
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Port (make sure port is open):");
+        int port = in.nextInt();
+
+        server s = new server(port);
     }
 }
