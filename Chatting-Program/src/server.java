b4c48956eab3721034fc434867ba79223c4fecaa
Caleb Graham <graham.caleb.m@gmail.com>
2025-12-03 22:16:50 -0600

    Server 2.4 server working

--- Chatting-Program/src/server.java
+++ Chatting-Program/src/server.java
@@ -1,68 +1,48 @@
 import java.net.*;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
+import java.util.*;
+import java.util.concurrent.*;
 
 public class server {
-
-
-    // Initialize socket and input stream
-    private Socket s = null;
-    private ServerSocket ss = null;
-    private DataInputStream in = null;
-    private DataOutputStream out = null;
     private static final ExecutorService pool = Executors.newCachedThreadPool();
+    private static final List<ClientHandler> clients = Collections.synchronizedList(new ArrayList<>());
     private static int clientCount = 0;
-    private static final List<ClientHandler> clientHandlers = Collections.synchronizedList(new ArrayList<>());
 
-
-    // Try to get machine's IP address, if fail throw exception
     private static String getIP() {
-        String ipString = null;
         try {
-            InetAddress ipAddress = InetAddress.getLocalHost();
-            ipString = ipAddress.getHostAddress();
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            return "Unknown";
         }
-        // Catch Unknown Host
-        catch (UnknownHostException e) {
-            System.err.println("Could not determine local host information: " + e.getMessage());
-            e.printStackTrace();
-        }
-        return ipString;
     }
 
-
-    // Try to get machine's Host name, if fail throw exception
     private static String getHost() {
-        String hostname = null;
         try {
-            InetAddress ipAddress = InetAddress.getLocalHost();
-            hostname = ipAddress.getHostName();
+            return InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            return "Unknown";
         }
-        // Catch Unknown Host
-        catch (UnknownHostException e) {
-            System.err.println("Could not determine local host information: " + e.getMessage());
-            e.printStackTrace();
-        }
-        return hostname;
     }
-
 
     private static class ClientHandler implements Runnable {
         private Socket socket;
+        private int clientId;
         private DataInputStream in;
         private DataOutputStream out;
-        private int clientId;
-        private boolean running = true;
 
         public ClientHandler(Socket socket, int clientId) {
             this.socket = socket;
             this.clientId = clientId;
+        }
+
+        public void sendMessage(String message) {
+            try {
+                if (out != null) {
+                    out.writeUTF(message);
+                }
+            } catch (IOException e) {
+                System.out.println("Error sending to Client #" + clientId);
+            }
         }
 
         @Override
@@ -72,85 +52,83 @@ public class server {
                 out = new DataOutputStream(socket.getOutputStream());
 
                 // Send welcome message
-                out.writeUTF("Welcome to Multi-Client Chat Server! You are Client #" +
-                        clientId + ". Type 'exit' to disconnect.");
+                out.writeUTF("Welcome! You are Client #" + clientId);
 
-                System.out.println("There are currently " + (clientHandlers.size() - 1) + " other clients connected.");
+                System.out.println("Client #" + clientId + " connected from " +
+                        socket.getInetAddress().getHostAddress());
 
-                System.out.println("Client #" + clientId + " handler started.");
+                // Syncronized is a lock so that clients isn't changed while being added to
+                synchronized(clients) {
+                    clients.add(this);
+                }
 
-                broadcast("Client #" + clientId + " has joined the chat.", this);
+                // Notify all clients (including self) that new client joined
+                broadcast("Client #" + clientId + " has joined the chat", this);
 
                 String message;
                 while (true) {
-                    message = in.readUTF();
+                    try {
+                        // Take message
+                        message = in.readUTF();
 
-                    if (message.equalsIgnoreCase("exit")) {
+                        // Disconnect
+                        if (message.equalsIgnoreCase("exit")) {
+                            System.out.println("Client #" + clientId + " disconnected.");
+                            break;
+                        }
+
+                        // Log message & client #
+                        System.out.println("Client #" + clientId + ": " + message);
+
+                        // Broadcast to ALL clients (including sender)
+                        broadcast("Client #" + clientId + ": " + message, null);
+
+                    } catch (EOFException e) {
+                        System.out.println("Client #" + clientId + " disconnected unexpectedly.");
                         break;
                     }
-
-                    String broadcastMsg = "Client #" + clientId + ": " + message;
-                    System.out.println(broadcastMsg);
-                    broadcast(broadcastMsg, null); // Send to everyone including sender
                 }
 
-                removeClient(this);
-                broadcast("Client #" + clientId + " has left the chat.", null);
+            }
+            // catch error
+            catch (IOException e) {
+                System.out.println("Client #" + clientId + " error: " + e.getMessage());
+            }
+            // remove client when leave
+            finally {
+                // Remove from list
+                synchronized(clients) {
+                    clients.remove(this);
+                }
+                // Notify remaining clients
+                broadcast("Client #" + clientId + " has left the chat", null);
 
-                out.writeUTF("Goodbye! Connection closed.");
-
-            } catch (IOException e) {
-                System.out.println("Client #" + clientId + " connection error: " + e.getMessage());
-            } finally {
-                // Close resources
+                // Close client's stuff
                 try {
-                    if (in != null) in.close();
-                    if (out != null) out.close();
-                    if (socket != null) socket.close();
-                } catch (IOException e) {
+                    in.close();
+                    out.close();
+                    socket.close();
+                }
+
+                // Error closing client
+                catch (IOException e) {
                     System.out.println("Error closing resources for Client #" + clientId);
                 }
             }
         }
-
-        public void sendMessage(String message) {
-            if (!running) return;
-            try {
-                out.writeUTF(message);
-            } catch (IOException e) {
-                System.out.println("Error sending to Client #" + clientId + ": " + e.getMessage());
-                running = false;
-            }
-        }
-
-        private void closeResources() {
-            running = false;
-            try {
-                if (in != null) in.close();
-                if (out != null) out.close();
-                if (socket != null) socket.close();
-            } catch (IOException e) {
-                System.out.println("Error closing resources for Client #" + clientId);
-            }
-        }
-
-        public int getClientId() {
-            return clientId;
-        }
     }
 
-    private static synchronized void addClient(ClientHandler client) {
-        clientHandlers.add(client);
-    }
 
-    private static synchronized void removeClient(ClientHandler client) {
-        clientHandlers.remove(client);
-        System.out.println("Client #" + client.getClientId() + " disconnected. " +
-                clientHandlers.size() + " client(s) remaining.");
-    }
-
-    private static synchronized void broadcast(String message, ClientHandler exclude) {
-        for (ClientHandler client : clientHandlers) {
+    // Broadcast to all users
+    private static void broadcast(String message, ClientHandler exclude) {
+        List<ClientHandler> clientsCopy;
+        // Lock clients while broadcasting
+        synchronized(clients) {
+            clientsCopy = new ArrayList<>(clients);
+        }
+        // send to all
+        for (ClientHandler client : clientsCopy) {
+            // If exclude is not null, skip that client
             if (exclude == null || client != exclude) {
                 client.sendMessage(message);
             }
@@ -158,7 +136,6 @@ public class server {
     }
 
     public static void main(String args[]) {
-        // Print IP address & hostname
         System.out.println("Chat Server");
         System.out.println("Your IP is: " + getIP());
         System.out.println("Your Hostname is: " + getHost());
@@ -168,35 +145,21 @@ public class server {
         int port = scanner.nextInt();
         scanner.close();
 
-        ServerSocket serverSocket = null;
-
         try {
-            serverSocket = new ServerSocket(port);
+            ServerSocket serverSocket = new ServerSocket(port);
             System.out.println("\nServer started on port " + port);
-            System.out.println("Waiting for client connections...");
-            System.out.println("Press Ctrl+C to stop the server.\n");
+            System.out.println("Waiting for clients...\n");
 
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 clientCount++;
 
-                System.out.println("New client #" + clientCount + " connected from: " + clientSocket.getInetAddress().getHostAddress());
-
-                ClientHandler clientHandler = new ClientHandler(clientSocket, clientCount);
-                addClient(clientHandler);
-                pool.execute(clientHandler);
+                ClientHandler handler = new ClientHandler(clientSocket, clientCount);
+                pool.execute(handler);
             }
 
         } catch (IOException e) {
             System.out.println("Server error: " + e.getMessage());
-        } finally {
-            // Clean up
-            pool.shutdown();
-            try {
-                if (serverSocket != null) serverSocket.close();
-            } catch (IOException e) {
-                System.out.println("Error closing server socket: " + e.getMessage());
-            }
         }
     }
 }
